7f3ee66b1f00100565ec59558deb3ad7c1542de9
Jean-Marc Andre <jandre@desmojim.fr>
2021-02-16 23:12:29 +0100

    cleaned volume command

--- pkg/cmd/volume.go
+++ pkg/cmd/volume.go
@@ -55,13 +55,6 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		paths, _ := cmd.Flags().GetStringSlice("path")
-		tag, _ := cmd.Flags().GetString("tag")
-		if err := backup.BackupVolume(tag, paths); err != nil {
-			os.Exit(1)
-		}
-	},
 }
 
 func init() {
